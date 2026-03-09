1225fb8d336ddfc8eb57152d5408449183197dda
kavikamkavikam562-creator <kavikamkavikam562@gmail.com>
2026-03-09 18:48:03 +0530

    Add files via upload

---
+++ Operators13.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Operators13 {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        int connection = scan.nextInt();
+        int appliance = scan.nextInt();
+
+                  
+
+    }
+}
