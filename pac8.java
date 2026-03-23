c3d52797a7d0ee3ca33f1fadc398bd77bec303cf
kavikamkavikam562-creator <kavikamkavikam562@gmail.com>
2026-03-23 18:59:40 +0530

    Add files via upload

---
+++ pac8.java
@@ -0,0 +1,19 @@
+public class pac8{
+    public static void main(String[] args) {
+
+        for (int num = 2; num <= 50; num++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(num);
+            }
+        }
+    }
+}
