9123dbd1c5ae848192e0ca78af361660165721b4
kavikamkavikam562-creator <kavikamkavikam562@gmail.com>
2026-04-16 22:45:39 +0530

    Add files via upload

---
+++ pac22.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> list = new ArrayList<>();
+
+        while(sc.hasNextInt()) {   
+            list.add(sc.nextInt());
+        }
+
+        System.out.println(list);
+    }
+}
