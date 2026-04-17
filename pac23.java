d80257b0471396a075886f27b32eaaa8cf83a898
kavikamkavikam562-creator <kavikamkavikam562@gmail.com>
2026-04-17 21:56:33 +0530

    Add files via upload

---
+++ pac23.java
@@ -0,0 +1,11 @@
+class Solution {
+    void helloFunction(){
+        System.out.print("Hello");
+    }
+    public static void main(String[] args){
+       Solution obj = new Solution();
+       obj.helloFunction();
+        
+    }
+    
+}
